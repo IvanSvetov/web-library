f08374af29f5d4b8dd4839c7df1a37ca0c9af4b3
Ivan Svetov <ivan_lbov@mail.ru>
2023-05-03 22:14:24 +0500

    Initial commit

---
+++ src/main/java/skypro/java/course4/weblibrary/WebLibraryApplication.java
@@ -0,0 +1,13 @@
+package skypro.java.course4.weblibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/java/course4/weblibrary/WebLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.java.course4.weblibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
